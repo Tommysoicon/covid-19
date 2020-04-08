571cec623c4485d76392dd565f80b8eebca37575
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2020-04-08 21:44:11 +0700

    Final
    final Covid-19

--- blog2/Chart.js
+++
@@ -1,8 +0,0 @@
-$.getJSON("https://corona.lmao.ninja/all", function (corona)
-{
-    $.each(corona, function (key, value) {
-        document.getElementById('TG1').innerHTML ='+ Infection: ' + corona.cases;
-        document.getElementById('TG2').innerHTML ='+ Case Die: ' +corona.deaths;
-        document.getElementById('TG3').innerHTML ='+ Recovered: ' +corona.recovered;
-    });
-});
